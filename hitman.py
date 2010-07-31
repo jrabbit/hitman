e46bda65b5e514cf64d699f4ad934986b48d2bc3
Jrabbit <jackjrabbit+github@gmail.com>
2010-07-30 23:00:47 -0500

    remove YAML. Start fleshing out DB work

--- hitman.py
+++ hitman.py
@@ -1,8 +1,8 @@
 #Hitman. Or The Professional. (c) 2010 Jrabbit Under GPL v3 or later.
-import yaml
 import feedparser
 import sys
 import os
+import anydbm
 import urlparse
 from urlgrabber.grabber import URLGrabber
 
@@ -21,7 +21,9 @@ def put_a_hit_out(name):
 
 def hitsquad():
     """"\'put a hit out\' on all known rss feeds"""
-    pass
+    feeds = get_feeds()
+    for name, feed in feeds.iteritems():
+        put_a_hit_out(name)
 
 def growl():
     """send a growl notification if on mac osx (use GNTP or the growl lib)"""
@@ -37,23 +39,37 @@ def growl():
 
 def download(url):
     """should do continues"""
+    db = anydbm.open('downloads', 'c')
     g = URLGrabber(reget='simple') #Donno if this is sane/works.
-    data = g.urlgrab(url)
+    try:
+        data = g.urlgrab(url)
+    except:
+        db[url] = 'Error'
     save_name = urlparse.urlparse(url) 
     # get jsut a file.ext
     f = open(os.path.join(dl_folder, feed_name, save_name), 'w')
     f.write(data)
+    db[url] = 'Downloaded'
 
-def add_feed(url):
+def add_feed(url, name):
     """add to yaml config file or db"""
-    pass
+    db = anydbm.open('feeds', 'c')
+    db[name] = url
+    db.close()
+
+def alias_feed(name, alias):
+    """write aliases to db"""
+    db = anydbm.open('aliases', 'c')
+    db[alias] = name
+    db.close()
 
 def get_feeds():
     """read out all feed information,
     return as a dictionary indexed by feed name proper"""
-    pass
+    db = anydbm.open('feeds', 'c')
+    return db
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        addfeed(sys.argv[1])
+    if len(sys.argv) > 2:
+        addfeed(sys.argv[1], sys.argv[2])
     hitsquad()
