fd2808ed8822f523f2cd20325e26238201f4d2c4
Jrabbit <jackjrabbit+github@gmail.com>
2011-01-14 19:13:20 -0600

    put manage logic into function so hitman can optionally use it

--- manage.py
+++ manage.py
@@ -15,22 +15,24 @@ def is_file_latest(file, url):
         else:
             return False
 
+def clean_downloads():
+    settings = hitman.get_settings()
+       if 'dl' in settings:
+           dl_dir = settings['dl']
+       else:
+           dl_dir = os.path.join(os.path.expanduser("~"), "Downloads")
+       listing = os.listdir(dl_dir)
+       for localfile,data in  hitman.get_downloads().iteritems():
+           # db[localfile] = JSON obj {'url': url,
+           # 'date': time.ctime(), 'feed': feed_url}
+           if localfile in listing:
+               values = json.loads(data)
+               #check if file is the latest
+               if is_file_latest(localfile, values['feed']):
+                   pass
+               else:
+                   os.remove(os.path.join(dl_dir, localfile))    
+
 
 if __name__ == "__main__":
-    settings = hitman.get_settings()
-    if 'dl' in settings:
-        dl_dir = settings['dl']
-    else:
-        dl_dir = os.path.join(os.path.expanduser("~"), "Downloads")
-    listing = os.listdir(dl_dir)
-    for localfile,data in  hitman.get_downloads().iteritems():
-        # db[localfile] = JSON obj {'url': url,
-        # 'date': time.ctime(), 'feed': feed_url}
-        if localfile in listing:
-            values = json.loads(data)
-            #check if file is the latest
-            if is_file_latest(localfile, values['feed']):
-                pass
-            else:
-                os.remove(os.path.join(dl_dir, localfile))
-
+   clean_downloads()
