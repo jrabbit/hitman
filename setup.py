c722b0fe56af89c1bc5f7729b6cf3a174258b1cf
Jrabbit <jackjrabbit+github@gmail.com>
2011-03-04 17:57:18 -0600

    fix'd deps

--- setup.py
+++ setup.py
@@ -7,9 +7,8 @@ setup(
     url='https://github.com/jrabbit/hitman',
     license='GPL v3',
     long_description="Hitman: The professional RSS enclosure downloader. Supports OPML. Supports various downloads methods: system wget, system curl, urlgrabber (pycurl based).",
-    py_modules = ['BeautifulSoup', 'feedparser'],
     scripts= ['hitman.py'],
     packages=['urlgrabber'],
-    install_requires=['pycurl'],
+    install_requires=['pycurl', 'feedparser', 'BeautifulSoup'],
     classifiers=['Environment :: Console', 'License :: OSI Approved :: GNU General Public License (GPL)', 'Topic :: Internet :: WWW/HTTP']
 )
