d2f6b27d7d65b23b8f7b6420aca9ed49b8940db3
Jrabbit <jackjrabbit+github@gmail.com>
2011-09-06 14:06:18 -0400

    fix some of the setup.py issues

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
-from distribute_setup import use_setuptools
-use_setuptools()
 from setuptools import setup
 setup(
     name='hitman',
-    version='1.0.1dev',
+    version='1.0.3dev',
     author='jrabbit',
     author_email='jackjrabbit+hitman@gmail.com',
     url='https://github.com/jrabbit/hitman',
     license='GPL v3',
     long_description="Hitman: The professional RSS enclosure downloader. Supports OPML. Supports various downloads methods: system wget, system curl, urlgrabber (pycurl based).",
+    description="The professional RSS enclosure downloader.",
     scripts= ['hitman.py'],
     packages=['urlgrabber'],
-    install_requires=['pycurl', 'feedparser', 'BeautifulSoup'],
+    install_requires=['pycurl', 'feedparser', 'BeautifulSoup', 'baker'],
     classifiers=['Environment :: Console', 'License :: OSI Approved :: GNU General Public License (GPL)', 'Topic :: Internet :: WWW/HTTP', 'Operating System :: BeOS','Topic :: Multimedia' ]
 )
 
