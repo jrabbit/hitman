f36e30485bf2aea6bab1e6a1e05a0d405f44c204
Jrabbit <jackjrabbit+github@gmail.com>
2012-05-03 16:13:51 -0400

    version bump!

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='hitman',
-    version='1.0.3dev',
+    version='1.1',
     author='jrabbit',
     author_email='jackjrabbit+hitman@gmail.com',
     url='https://github.com/jrabbit/hitman',
