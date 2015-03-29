6eb5658ee90040c513567dad5e7aad68d62f8b47
jrabbit <jackjrabbit+github@gmail.com>
2015-03-28 22:23:22 -0400

    more troves, clarify license, remove references to wget/curl

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ setup(
     author_email='jackjrabbit+hitman@gmail.com',
     url='https://github.com/jrabbit/hitman',
     license='GPL v3',
-    long_description="Hitman: The professional RSS enclosure (Podcast) downloader. Supports OPML. Supports various downloads methods: system wget, system curl, urlgrabber (pycurl based).",
+    long_description="Hitman: The professional RSS enclosure (Podcast) downloader. Supports OPML. Supports various downloads methods: requests and urlgrabber (pycurl based).",
     description="The professional RSS enclosure downloader.",
     scripts= ['hitman.py'],
-    packages=['urlgrabber'],
     install_requires=['pycurl', 'feedparser', 'beautifulsoup4', 'baker==1.1', 'gntplib'],
-    classifiers=['Environment :: Console', 'License :: OSI Approved :: GNU General Public License (GPL)', 'Topic :: Internet :: WWW/HTTP', 'Operating System :: BeOS','Topic :: Multimedia' ]
+    classifiers=['Environment :: Console', 
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)', 
+    'Topic :: Internet :: WWW/HTTP', 'Operating System :: BeOS',
+    'Topic :: Multimedia', 'Operating System :: OS Independent', 'Programming Language :: Python :: 2 :: Only' ]
 )
 
