2049877cb7293542214f695ff94c300a7c75c286
jrabbit <jackjrabbit+github@gmail.com>
2015-04-14 14:05:50 -0400

    Add versions to requires closes #20

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='hitman',
-    version='1.2a2',
+    version='1.2a3',
     author='jrabbit',
     author_email='jackjrabbit+hitman@gmail.com',
     url='https://github.com/jrabbit/hitman',
@@ -9,7 +9,7 @@ setup(
     long_description="Hitman: The professional RSS enclosure (Podcast) downloader. Supports OPML. Supports various downloads methods: requests and urlgrabber (pycurl based).",
     description="The professional RSS enclosure downloader.",
     scripts= ['hitman.py'],
-    install_requires=['pycurl', 'feedparser', 'beautifulsoup4', 'baker==1.1', 'gntplib'],
+    install_requires=['pycurl>=7.19.5.1', 'feedparser>=5.1.3', 'beautifulsoup4>=4.3.2', 'baker==1.3', 'gntplib>=0.5'],
     classifiers=['Environment :: Console', 
     'License :: OSI Approved :: GNU General Public License v3 (GPLv3)', 
     'Topic :: Internet :: WWW/HTTP', 'Operating System :: BeOS',
